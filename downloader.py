92043a3b8d5789842da67d6331f24902e2e8db54
Ednaordinary <88869424+Ednaordinary@users.noreply.github.com>
2024-06-21 04:44:18 -0600

    everything works, spotify downloads are a bit slow for some reason though

--- downloader.py
+++ downloader.py
@@ -1,10 +1,13 @@
-import yt_dlp
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+import os
+import re
+import sys
 import json
+import time
+import spotipy
 import requests
 import threading
-yt_dlp.utils.bug_reports_message = lambda: ''
+from yt_dlp import YoutubeDL
+from spotipy.oauth2 import SpotifyOAuth
 playlist_downloads = {}
 
 def authenticate_spotify(spotify_id, spotify_secret):
@@ -58,7 +61,7 @@ def get_from_url_youtube(url, path):
         'format': 'bestaudio/best',
         'extractaudio': True,
         'audioformat': 'best',
-        'outtmpl': path,
+        'outtmpl': path+".mp3",
         'restrictfilenames': True,
         'noplaylist': True,
         'nocheckcertificate': True,
@@ -96,20 +99,31 @@ def get_from_search_youtube(term, path):
     }
     try:
         with YoutubeDL(YTDL_OPTIONS) as dl:
-            info = dl.extract_info(f"ytsearch1:{arg}", download=False)['entries']
+            print("getting info")
+            info = dl.extract_info(f"ytsearch1:{term}", download=False)['entries']
+            print("done")
     except:
         return False
     else:
-        return True
-    if info is not None:
-        url = "https://www.youtube.com/watch?v="+info[0]['id']
-        return get_from_url_youtube(url, path)
-    else:
-        return False
+        if info:
+            url = "https://www.youtube.com/watch?v="+info[0]['id']
+            return get_from_url_youtube(url, path)
+        else:
+            return False
 
 def playlist_assembler(url, path_local, path_global):
     global playlist_downloads
     content = get_from_url_youtube(url, path_local)
+    if content:
+        playlist_downloads[path_global].append(path_local)
+    else:
+        playlist_downloads[path_global].append(None)
+
+def playlist_assembler_spotify(term, path_local, path_global):
+    print("assembler started")
+    global playlist_downloads
+    content = get_from_search_youtube(term, path_local)
+    print("search done")
     if content:
         playlist_downloads[path_global].append(path_local)
     else:
@@ -134,31 +148,84 @@ def is_playlist(url):
         'extract_flat': True,
     }
     with YoutubeDL(YTDL_OPTIONS) as dl:
-        content = ydl.extract_info(arg, download=False)
+        content = dl.extract_info(url, download=False)
     if 'entries' in content:
-        return True
+        return content
     else:
         return False
 
-def get_from_url(url, path, playlist):
-    if playlist:
+def get_from_url(url, path):
+    #try:
+    content = is_playlist(url)
+    if content:
         global playlist_downloads
         playlist_downloads[path] = []
         threads = []
+        paths = []
         for idx, entry in enumerate(content['entries']):
             threads.append(threading.Thread(target=playlist_assembler, args=["https://www.youtube.com/watch?v="+entry['id'], path+'-'+str(idx), path]))
+            paths.append(path+'-'+str(idx))
         for thread in threads:
             thread.start()
+            time.sleep(0.5)
         for thread in threads:
             thread.join()
-        return playlist_downloads[path]
+        return_value = []
+        for value in paths: # sort them to be in order
+            if value in playlist_downloads[path]:
+                return_value.append(value)
+            else:
+                return_value.append(None)
+        del playlist_downloads[path]
+        return return_value
     else:
         status = get_from_url_youtube(url, path_local)
         if status:
             return [path]
         else:
             return [None]
+    #except:
+    #    return [None]
 
-def get_from_term(term, path):
-    response = requests.options(term)
-    
+class fake_response:
+    status_code = 404
+
+def get_from_term(term, path, sp):
+    try:
+        response = requests.options(term)
+    except:
+        response = fake_response
+    if response.status_code != 404:
+        if re.compile(r"((?:www|open)\.)?((?:spotify\.com))").search(term):
+            result = get_from_url_spotify(term, sp)
+            if result:
+                threads = []
+                paths = []
+                global playlist_downloads
+                playlist_downloads[path] = []
+                for idx, track in enumerate(result):
+                    threads.append(threading.Thread(target=playlist_assembler_spotify, args=[track, path+'-'+str(idx), path]))
+                    paths.append(path+'-'+str(idx))
+                for thread in threads:
+                    thread.start()
+                    time.sleep(0.1)
+                for thread in threads:
+                    thread.join()
+                return_value = []
+                for value in paths: # sort them to be in order
+                    if value in playlist_downloads[path]:
+                        return_value.append(value)
+                    else:
+                        return_value.append(None)
+                del playlist_downloads[path]
+                return return_value
+            else:
+                return None
+            return spotify_handler(term, interaction, channel)
+        else:
+            return get_from_url(term, path)
+    else:
+        if get_from_search_youtube(term, path):
+            return [path]
+        else:
+            return [None]
