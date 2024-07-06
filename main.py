164272fbf2a9b95121e09b7734f2ed2dd4fb61f8
BuildTools <imnotednamode@gmail.com>
2024-07-06 01:41:05 -0600

    glitchy right now. initial remove button implementation

--- main.py
+++ main.py
@@ -16,20 +16,18 @@ sp = authenticate_spotify(SPOTIFY_ID, SPOTIFY_TOKEN)
 os.makedirs("./music", exist_ok=True)
 
 session_channels = {}
-
-#
-#async for i in get_from_term(term, path, sp):
-#     print(i)
+current_song_message = {}
 
 def channel_watcher(guild):
     global session_channels
+    global current_song_message
     proto = session_channels[guild][2]
     timeout = time.time() + 300
     while True:
         time.sleep(0.01)
         if time.time() > timeout and timeout != 0:
             asyncio.run_coroutine_threadsafe(coro=proto.disconnect(), loop=client.loop)
-            asyncio.run_coroutine_threadsafe(coro=session_channels[0].send("No songs played in the last 5 minutes, disconnected!\nStart a new session to keep playing"), loop=client.loop)
+            asyncio.run_coroutine_threadsafe(coro=session_channels[guild].send("No songs played in the last 5 minutes, disconnected!\nStart a new session to keep playing"), loop=client.loop)
             del session_channels[guild]
             return
         try:
@@ -43,7 +41,8 @@ def channel_watcher(guild):
             else:
                 source = discord.FFmpegOpusAudio(session_channels[guild][3][0][0]+".mp3", bitrate=256)
                 session_channels[guild][2].play(source)
-                asyncio.run_coroutine_threadsafe(coro=guild.me.edit(nick="Playing " + session_channels[guild][3][0][1][-24:]), loop=client.loop)
+                asyncio.run_coroutine_threadsafe(coro=guild.me.edit(nick="Playing " + session_channels[guild][3][0][1][:-24]), loop=client.loop)
+                asyncio.run_coroutine_threadsafe(coro=add_next_button(current_song_message[guild.id][0], guild), loop=client.loop)
                 try:
                     while session_channels[guild][2].is_playing():
                         time.sleep(0.01)
@@ -52,6 +51,8 @@ def channel_watcher(guild):
                 session_channels[guild][3].pop(0)
                 timeout = time.time() + 300
                 asyncio.run_coroutine_threadsafe(coro=guild.me.edit(nick=None), loop=client.loop)
+                asyncio.run_coroutine_threadsafe(coro=remove_view(current_song_message[guild.id][0]), loop=client.loop)
+                current_song_message[guild.id].pop(0)
 
 class MusicActions(discord.ui.View):
     def __init__(self, *, timeout=None, guild):
@@ -81,6 +82,35 @@ class MusicActions(discord.ui.View):
         proto.stop()
         await interaction.response.pong()
 
+class NextButton(discord.ui.View):
+    def __init__(self, *, timeout=None, guild):
+        super().__init__(timeout=timeout)
+        self.guild = guild
+    @discord.ui.button(label="Next", style=discord.ButtonStyle.primary)
+    async def next_button(self, button: discord.ui.Button, interaction: discord.Interaction):
+        global session_channels
+        proto = session_channels[self.guild][2]
+        queue = session_channels[self.guild][3]
+        proto.stop()
+        await interaction.response.edit_message(view=None)
+
+class RemoveButton(discord.ui.View):
+    def __init__(self, *, timeout=None, song, guild):
+        super().__init__(timeout=timeout)
+        self.song = song
+        self.guild = guild
+    @discord.ui.button(label="Remove", style=discord.ButtonStyle.red)
+    async def remove_button(self, button: discord.ui.Button, interaction: discord.Interaction):
+        try:
+            song_index = session_channels[self.guild][3].index(self.song)
+            if song_index:
+                session_channels[self.guild][3].pop(song_index)
+                await interaction.response.edit_message(content="Removed " + str(self.song[1]) + " from the queue.")
+            else:
+                await interaction.response.edit_message(content="Song not found in the queue.")
+        except:
+            await interaction.response.edit_message(content="Song not found in the queue.")
+
 @client.event
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
@@ -91,9 +121,10 @@ async def on_ready():
 @client.slash_command(description="Starts a music session")
 async def session(
         interaction: discord.Interaction,
-        channel: discord.VoiceChannel, #discord.abc.GuildChannel,
+        channel: discord.VoiceChannel,
 ):
     global session_channels
+    global current_song_message
     try:
         session_channels[interaction.guild]
     except:
@@ -107,14 +138,25 @@ async def session(
         await thread.send("Send a link or search term here to start!")
         proto = await channel.connect()
         session_channels[interaction.guild] = (thread, channel, proto, [])
+        current_song_message[interaction.guild.id] = []
         threading.Thread(target=channel_watcher, args=[interaction.guild]).start()
         await original_message.edit(view=MusicActions(guild=interaction.guild))
     else:
         await interaction.response.send_message("This guild already has a session in progress!\n" + str(session_channels[interaction.guild][0].jump_url))
 
-async def song_send(message, name, path, delete):
-    await message.channel.send(name, file=discord.File(fp=path+".mp3", filename=name+".mp3"))
+async def song_send(message, name, path, delete, song, guild):
+    global current_song_message
+    if current_song_message[guild.id] == []:
+        current_song_message[guild.id].append(await message.channel.send(name, file=discord.File(fp=path+".mp3", filename=name+".mp3"), view=NextButton(guild=guild)))
+    else:
+        current_song_message[guild.id].append(await message.channel.send(name, file=discord.File(fp=path + ".mp3", filename=name + ".mp3"), view=RemoveButton(song=song, guild=guild)))
     if delete: os.remove(path+".mp3")
+
+async def add_next_button(message, guild):
+    await message.edit(view=NextButton(guild=guild))
+
+async def remove_view(message):
+    await message.edit(view=None)
 
 async def async_downloader(term, path, message):
     global session_channels
@@ -126,13 +168,13 @@ async def async_downloader(term, path, message):
                     session_channels[message.guild][3].append(song)
             except:
                 continue_queue = False
-                asyncio.run_coroutine_threadsafe(coro=song_send(message, song[1], song[0], True), loop=client.loop)
+                asyncio.run_coroutine_threadsafe(coro=song_send(message, song[1], song[0], True, song, message.guild), loop=client.loop)
                 pass
             else:
                 if continue_queue:
-                    asyncio.run_coroutine_threadsafe(coro=song_send(message, song[1], song[0], False), loop=client.loop)
+                    asyncio.run_coroutine_threadsafe(coro=song_send(message, song[1], song[0], False, song, message.guild), loop=client.loop)
                 else:
-                    asyncio.run_coroutine_threadsafe(coro=song_send(message, song[1], song[0], True), loop=client.loop)
+                    asyncio.run_coroutine_threadsafe(coro=song_send(message, song[1], song[0], True, song, message.guild), loop=client.loop)
         else:
             asyncio.run_coroutine_threadsafe(coro=message.channel.send("Something went wrong while downloading a song!"), loop=client.loop)
 
